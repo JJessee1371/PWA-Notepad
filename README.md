a9879866035d5291df236bf98f9de3775178f7e4
Jon Jessee <tjessee7624@gmail.com>
2020-11-22 12:01:49 -0700

    Updated README.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # PWA-Notepad
+
+## Description
+Note taking progressive web application that allows the user to operate the app
+both online and offline in addition to being able to use it in its own standalone
+window. 
+## Screenshot
+![mongo-notepad](https://user-images.githubusercontent.com/66571617/99914381-4fd79600-2cba-11eb-9512-e0ce127c0661.PNG)
+## Technologies
+* HTML
+* CSS
+* JavaScript
+* Express
+* Morgan
+* MongoDB
+* Mongojs
+* Nodemon
+
+## Deployed Apllication
+
+## Credits
+The UI and front end capability were provided by the University of Utah coding
+bootcamp. 
--- public/sw.js
+++ public/sw.js
@@ -2,21 +2,18 @@ const fileCache = 'file-v1';
 
 const dataCache = 'data-v1';
 
+const iconSizes = ['72', '96', '128', '144', '152', '192', '384', '512'];
+const iconFiles = iconSizes.map(
+    (size) => `/assets/images/icons/icon-${size}x${size}.png`
+);
+
 const filesToCache = [
     '/',
     '/app.js',
     '/favicon.ico',
     '/index.html',
     '/manifest.webmanifest',
-    '/assets/images/icons/icon-72x72.png',
-    '/assets/images/icons/icon-96x96.png',
-    '/assets/images/icons/icon-128x128.png',
-    '/assets/images/icons/icon-144x144.png',
-    '/assets/images/icons/icon-152x152.png',
-    '/assets/images/icons/icon-192x192.png',
-    '/assets/images/icons/icon-384x384.png',
-    '/assets/images/icons/icon-512x512.png'
-];
+].concat(iconFiles);
 
 //Cache static assets on install event
 self.addEventListener('install', (e) => {
